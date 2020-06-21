77817a1265800c9fd791b4c395f85b9d53a92b05
Ansh Gujral <anshgujral1997@gmail.com>
2020-06-22 00:12:06 +0530

    add complex
    Signed-off-by: Ansh Gujral <anshgujral1997@gmail.com>

---
+++ complex/README.md
@@ -0,0 +1,5 @@
+## Dev setup
+
+1. `docker-compose build`
+2. `docker-compose up`
+3. Open http://localhost:3030
---
+++ complex/client/src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import OtherPage from "./OtherPage";
+import Fib from "./Fib";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Link className="App-link" to="/">
+            Home
+          </Link>
+          <Link className="App-link" to="/otherpage">
+            Other Page
+          </Link>
+          <br />
+          <Route exact path="/" component={Fib} />
+          <Route exact path="/otherpage" component={OtherPage} />
+        </header>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
